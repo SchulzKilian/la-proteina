a08d69053d681714879f6e2fcd89dcc2250e1eeb
SchulzKilian <schulz.kilian@outlook.de>
2026-02-11 14:45:26 +0000

    optimising the script

--- script_utils/submit_train.sh
+++ script_utils/submit_train.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #SBATCH -J train_test
-#SBATCH -A COMPUTERLAB-SL3-GPU          # <--- CHANGE to your GPU account (e.g., COMPUTERLAB-SL3-GPU)
-#SBATCH -p ampere                # <--- GPU partition
-#SBATCH --gres=gpu:4             # Request 1 GPU
+#SBATCH -A COMPUTERLAB-SL2-GPU
+#SBATCH -p ampere
+#SBATCH --gres=gpu:4
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=24       # 16 CPUs for data loading
-#SBATCH --time=12:00:00          # 4 hours (adjust as needed)
-#SBATCH --output=slurm_train_%j.out
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=0                 # <--- FIX: Request ALL System RAM to feed the 4 GPUs
+#SBATCH --time=4:00:00
+#SBATCH
 
 # 1. Load your personal shell config (Reliable Conda setup)
 source $HOME/.bashrc
