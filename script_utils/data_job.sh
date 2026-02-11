a38f8a54dc8e956a7cbda6ea8d8ee2e6e95e3828
SchulzKilian <schulz.kilian@outlook.de>
2026-02-11 11:42:09 +0000

    less workers for the big files in

--- script_utils/data_job.sh
+++ script_utils/data_job.sh
@@ -3,8 +3,9 @@
 #SBATCH -A COMPUTERLAB-SL2-CPU   # Matches your -A
 #SBATCH -p sapphire              # Matches your -p
 #SBATCH --nodes=1
+#SBATCH --mem=64G
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=6       # Matches your --cpus-per-task
+#SBATCH --cpus-per-task=4       # Matches your --cpus-per-task
 #SBATCH --time=1:00:00          # I increased this to 4h (1h might be tight for downloading)
 #SBATCH --output=slurm_prep_%j.out
 source $HOME/.bashrc
