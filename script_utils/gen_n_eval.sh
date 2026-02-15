31bd7269e1618cc1d62898a73368b0a81b44724e
SchulzKilian <schulz.kilian@outlook.de>
2026-02-15 14:41:36 +0000

    have proper eval

--- script_utils/gen_n_eval.sh
+++ script_utils/gen_n_eval.sh
@@ -1,5 +1,8 @@
 CONFIG_NAME="inference_ucond_notri"
 
 rm -r inference  # This removes all your inference runs so far in that directory
-python proteinfoundation/generate.py --config_name $CONFIG_NAME
+python proteinfoundation/generate.py --config_name inference_ucond_notri \
+    ckpt_path=./store/test_release_diffusion/checkpoints \
+    ckpt_name=last.ckpt \
+    autoencoder_ckpt_path=./checkpoints_laproteina/AE1_ucond_512.ckpt
 python proteinfoundation/evaluate.py --config_name $CONFIG_NAME
