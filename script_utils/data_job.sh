5327a5a3a41d04302f8dafc1a9d3f81de0b19f62
SchulzKilian <kilianschulz@kilianschulz.com>
2026-03-16 13:32:35 +0000

    turning off threading

--- script_utils/data_job.sh
+++ script_utils/data_job.sh
@@ -3,7 +3,7 @@
 #SBATCH -A COMPUTERLAB-SL2-CPU   # Matches your -A
 #SBATCH -p icelake              # Matches your -p
 #SBATCH --nodes=1
-#SBATCH --mem=256G
+#SBATCH --mem=230G
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=12       # Matches your --cpus-per-task
 #SBATCH --time=1:30:00          # I increased this to 4h (1h might be tight for downloading)
